e2abcbc77d0dcc723d79b7542258c11d5e6554d3
nazma98 <nazmasarker1311@gmail.com>
2025-07-25 23:31:04 +0600

    Add createProduct service for creating new products

--- main.go
+++ main.go
@@ -38,6 +38,34 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 	encoder.Encode(productList)
 }
 
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != "POST" {
+		http.Error(w, "Please give me POST request", 400)
+		return
+	}
+
+	var newProduct Product
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&newProduct)
+
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "PLease give me valid json", 400)
+		return
+	}
+
+	newProduct.ID = len(productList) + 1
+
+	productList = append(productList, newProduct)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(newProduct)
+}
+
 func main() {
 	mux := http.NewServeMux()
 
@@ -46,6 +74,8 @@ func main() {
 	mux.HandleFunc("/about", aboutHandler)
 
 	mux.HandleFunc("/products", getProducts)
+
+	mux.HandleFunc("/create-products", createProduct)
 
 	fmt.Println("Server running on :8080")
 
@@ -80,35 +110,35 @@ func init() {
 		ImgURL:      "https://www.allrecipes.com/thmb/lc7nSL9L5zMHXz9t6PMAVm9biNM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ar-new-banana-adobe-ar-2x1-917fdde58d194b529b41042ebff1c031.jpg",
 	}
 
-	prd4 := Product{
-		ID:          3,
-		Title:       "Orange",
-		Description: "Orange is orange. I love orange.",
-		Price:       100,
-		ImgURL:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTct4QnA6zwU4tyn9vq_ADJ8Ey6RfIWK-6V6g&s",
-	}
+	// prd4 := Product{
+	// 	ID:          3,
+	// 	Title:       "Orange",
+	// 	Description: "Orange is orange. I love orange.",
+	// 	Price:       100,
+	// 	ImgURL:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTct4QnA6zwU4tyn9vq_ADJ8Ey6RfIWK-6V6g&s",
+	// }
 
-	prd5 := Product{
-		ID:          4,
-		Title:       "Apple",
-		Description: "Apple is green. I don't really like apple.",
-		Price:       180,
-		ImgURL:      "https://www.harrisfarm.com.au/cdn/shop/products/40715-done.jpg?v=1623908361&width=1920",
-	}
+	// prd5 := Product{
+	// 	ID:          4,
+	// 	Title:       "Apple",
+	// 	Description: "Apple is green. I don't really like apple.",
+	// 	Price:       180,
+	// 	ImgURL:      "https://www.harrisfarm.com.au/cdn/shop/products/40715-done.jpg?v=1623908361&width=1920",
+	// }
 
-	prd6 := Product{
-		ID:          5,
-		Title:       "Mango",
-		Description: "Mango is yummy. I love it very much.",
-		Price:       4565,
-		ImgURL:      "https://www.allrecipes.com/thmb/lc7nSL9L5zMHXz9t6PMAVm9biNM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ar-new-banana-adobe-ar-2x1-917fdde58d194b529b41042ebff1c031.jpg",
-	}
+	// prd6 := Product{
+	// 	ID:          5,
+	// 	Title:       "Mango",
+	// 	Description: "Mango is yummy. I love it very much.",
+	// 	Price:       4565,
+	// 	ImgURL:      "https://www.allrecipes.com/thmb/lc7nSL9L5zMHXz9t6PMAVm9biNM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ar-new-banana-adobe-ar-2x1-917fdde58d194b529b41042ebff1c031.jpg",
+	// }
 
 	productList = append(productList, prd1)
 	productList = append(productList, prd2)
 	productList = append(productList, prd3)
-	productList = append(productList, prd4)
-	productList = append(productList, prd5)
-	productList = append(productList, prd6)
+	// productList = append(productList, prd4)
+	// productList = append(productList, prd5)
+	// productList = append(productList, prd6)
 
 }
